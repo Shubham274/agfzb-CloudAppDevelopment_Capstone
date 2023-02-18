0d84f4f0f7ff271281c55915d29cba8d9dfb31ee
Skaushik274 <Skaushik274@gmail.com>
2023-02-18 16:20:11 -0500

    2nd week github changes done

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -15,14 +15,14 @@ urlpatterns = [
     path('contact/', views.contact, name="contact"),
 
     # path for registration
-
+    path('registration/', views.registration, name="registration"),
     # path for login
-
+    path('login/', views.login_request, name='login'),
     # path for logout
 
-    path(route='', view=views.get_dealerships, name='index'),
-
+    path('logout/', views.logout_request, name='logout'),
     # path for dealer reviews view
+    path(route='', view=views.get_dealerships, name='index'),
 
     # path for add a review view
 
